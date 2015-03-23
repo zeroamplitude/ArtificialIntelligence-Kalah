70b80e6593b4ffdf401adf4596b257aab302879b
ncdesouza <nicholas.desouza@uoit.net>
2015-03-23 13:38:35 -0400

    Creating Test Suite

---
+++ test/algorithmtestsuite/PlayerTest.java
@@ -0,0 +1,45 @@
+package algorithmtestsuite;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testSetTmpBoard() throws Exception {
+
+    }
+
+    @Test
+    public void testMakePlay() throws Exception {
+
+    }
+
+    @Test
+    public void testGetMoveArray() throws Exception {
+
+    }
+
+    @Test
+    public void testGetValueOfMove() throws Exception {
+
+    }
+
+    @Test
+    public void testMain() throws Exception {
+
+    }
+}
